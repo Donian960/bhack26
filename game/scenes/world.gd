96e0ee9ecc74ebd9ea5722d6bd041f460a153a36
unknown <samuel.wickstead@gmail.com>
2026-03-14 14:29:10 +0000

    updated function names

--- game/scenes/world.gd
+++ game/scenes/world.gd
@@ -2,24 +2,19 @@ extends Node2D
 
 func _ready():
 	get_random_article()
-	
-func _on_request_completed(result, response_code, headers, body):
-	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json)
 
 func get_random_article():
-	$HTTPRequest.request_completed.connect(random_article_completed)
+	$HTTPRequest.request_completed.connect(random_article_returned)
 	$HTTPRequest.request("https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&grnfilterredir=nonredirects&prop=pageviews&prop=images&format=json")
 
-func random_article_completed(result, response_code, headers, body):
+func random_article_returned(result, response_code, headers, body):
+	
 	var json = JSON.parse_string(body.get_string_from_utf8())
-	print(json)
-
 	var id = int(json["query"]["random"][0]["id"])
 	
-	$HTTPRequest2.request_completed.connect(random_data_completed)
+	$HTTPRequest2.request_completed.connect(page_data_returned)
 	$HTTPRequest2.request("https://en.wikipedia.org/w/api.php?action=query&pageids="+str(id)+"&prop=pageviews|images&format=json")
 
-func random_data_completed(result, response_code, headers, body):
+func page_data_returned(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	print(json)
