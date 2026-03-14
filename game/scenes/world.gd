ad9cd62c549ecc0e082366810782585918ba4f6b
unknown <samuel.wickstead@gmail.com>
2026-03-14 14:41:41 +0000

    image gotten

--- game/scenes/world.gd
+++ game/scenes/world.gd
@@ -34,7 +34,26 @@ func page_data_returned(result, response_code, headers, body):
 			image_data = json["query"]["pages"][id]["images"]
 		if json["query"]["pages"][id].has("thumbnail"):
 			thumbnail = json["query"]["pages"][id]["thumbnail"]
+			$HTTPRequest3.request_completed.connect(image_returned)
+			$HTTPRequest3.request(thumbnail["source"])
 		
 	print(view_data)
 	print(image_data)
 	print(thumbnail)
+
+func image_returned(result, response_code, headers, body):
+	
+	if result != HTTPRequest.RESULT_SUCCESS:
+		push_error("Image couldn't be downloaded. Try a different image.")
+		
+	var image = Image.new()
+	var error = image.load_jpg_from_buffer(body)
+	if error != OK:
+		push_error("Couldn't load the image.")
+
+	var texture = ImageTexture.create_from_image(image)
+
+	# Display the image in a TextureRect node.
+	var texture_rect = TextureRect.new()
+	add_child(texture_rect)
+	texture_rect.texture = texture
