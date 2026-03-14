672a3e80a6c3ba4c3ec7199b3b6be842f228d204
Donian960 <daniellawson960@gmail.com>
2026-03-14 14:15:43 +0000

    somethinf or other

--- game/scenes/world.gd
+++ game/scenes/world.gd
@@ -15,3 +15,12 @@ func get_random_article():
 func random_article_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	print(json)
+
+	var id = int(json["query"]["random"][0]["id"])
+	
+	$HTTPRequest2.request_completed.connect(random_data_completed)
+	$HTTPRequest2.request("https://en.wikipedia.org/w/api.php?action=query&pageids="+str(id)+"&prop=pageviews|images&format=json")
+
+func random_data_completed(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json)
