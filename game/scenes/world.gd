a34f22bca21ba8d775d1e5d114616b352921db90
unknown <samuel.wickstead@gmail.com>
2026-03-14 14:04:56 +0000

    random article

--- game/scenes/world.gd
+++ game/scenes/world.gd
@@ -2,9 +2,16 @@ extends Node2D
 
 
 func _ready():
-	$HTTPRequest.request_completed.connect(_on_request_completed)
-	$HTTPRequest.request("https://en.wikipedia.org/w/api.php?action=query&pageids=23795530&prop=pageviews&format=json")
-
+	get_random_article()
+	
 func _on_request_completed(result, response_code, headers, body):
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json)
+
+func get_random_article():
+	$HTTPRequest.request_completed.connect(random_article_completed)
+	$HTTPRequest.request("https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnfilterredir=nonredirects&format=json")
+
+func random_article_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	print(json)
