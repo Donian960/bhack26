16bee744369c76623b33e94e502b9138487c1b14
Donian960 <daniellawson960@gmail.com>
2026-03-14 14:32:57 +0000

    fucker

--- game/scenes/world.gd
+++ game/scenes/world.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var precard = preload("res://scenes/card.tscn")
 
 func _ready():
 	get_random_article()
@@ -19,8 +20,24 @@ func random_article_completed(result, response_code, headers, body):
 	var id = int(json["query"]["random"][0]["id"])
 	
 	$HTTPRequest2.request_completed.connect(random_data_completed)
-	$HTTPRequest2.request("https://en.wikipedia.org/w/api.php?action=query&pageids="+str(id)+"&prop=pageviews|images&format=json")
+	$HTTPRequest2.request("https://en.wikipedia.org/w/api.php?action=query&pageids="+str(id)+"&prop=pageviews|images|pageimages&format=json")
 
 func random_data_completed(result, response_code, headers, body):
 	var json = JSON.parse_string(body.get_string_from_utf8())
+	
+	var view_data
+	var image_data
+	var thumbnail
+	
 	print(json)
+	
+	for id in json["query"]["pages"]:
+		if json["query"]["pages"][id].has("pageviews"):
+			view_data = json["query"]["pages"][id]["pageviews"]
+		if json["query"]["pages"][id].has("images"):
+			image_data = json["query"]["pages"][id]["images"]
+		if json["query"]["pages"][id].has("thumbnail"):
+			thumbnail = json["query"]["pages"][id]["thumbnail"]
+		
+	print(view_data)
+	print(image_data)
