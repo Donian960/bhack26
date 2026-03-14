fd1fb9350e5e714552b6d39bab309215ea974088
unknown <samuel.wickstead@gmail.com>
2026-03-14 13:15:40 +0000

    stuff

--- game/scenes/world.gd
+++ game/scenes/world.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 func _ready():
 	$HTTPRequest.request_completed.connect(_on_request_completed)
-	$HTTPRequest.request("https://en.wikipedia.org/w/index.php?title=Pet_door&action=raw&format=json")
+	$HTTPRequest.request("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0")
 
 func _on_request_completed(result, response_code, headers, body):
-	print(body.get_string_from_utf8())
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	print(json)
